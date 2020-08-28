e25c5c3b7d84cc72a0f6a24c09af8764d0505e45
uile-alt <haotian908@gmail.com>
2020-08-29 02:07:51 +0800

    3

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ var path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.[contenthash].js",
