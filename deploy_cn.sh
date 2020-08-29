b17234dea03f2e81a2bed8b0ff1fb721952b6451
uile-alt <haotian908@gmail.com>
2020-08-29 17:41:08 +0800

    6

--- deploy_cn.sh
+++
@@ -1,9 +0,0 @@
-yarn build &&
-git checkout gh-pages &&
-rm -rf *.html *.js *.css *jpg &&
-mv dist/* ./ &&
-rm -rf dist ;
-git add . &&
-git commit -m "update" &&
-git push mayun gh-pages:master && 
-git checkout -
